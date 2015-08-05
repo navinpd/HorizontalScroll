dc99629674d516ab8f880f3c68616ce6da0d5feb
Navin <navin.prasad@accionlabs.com>
2015-08-05 18:32:04 +0800

    Code is updated toaccomodate search as well

--- app/src/main/java/com/example/navinpd/horizontalscrollview/MainActivity.java
+++ app/src/main/java/com/example/navinpd/horizontalscrollview/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,17 +20,20 @@ import java.util.ArrayList;
 
 /**
  * A linearlayout contains child views.
+ * Created by navin.pd@gmail.com on 5/8/15
  */
 public class MainActivity extends ActionBarActivity {
-    LinearLayout linearContainer;
-    Button addButton;
-    HorizontalScrollView horizontalScroll;
     RelativeLayout mainHolder;
-    TextView searchText;
-    ImageView iv;
+    RelativeLayout friendContainer;
+    LinearLayout linearContainer;
+    HorizontalScrollView horizontalScroll;
+    ImageView searchIcon;
+    RelativeLayout searchContainer;
+    SearchView searchText;
+    TextView searchCancel;
     int oldWidth;
     ArrayList<View> viewContainer;
-    View view;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +43,20 @@ public class MainActivity extends ActionBarActivity {
         linearContainer = (LinearLayout) findViewById(R.id.horizontal_linear);
         horizontalScroll = (HorizontalScrollView) findViewById(R.id.horizontal_Scroll);
         mainHolder = (RelativeLayout) findViewById(R.id.main_holder);
-        searchText = (TextView) findViewById(R.id.search_text);
+        friendContainer = (RelativeLayout) findViewById(R.id.friend_container);
+        searchIcon = (ImageView) findViewById(R.id.search_icon);
+
+        searchContainer = (RelativeLayout) findViewById(R.id.search_container);
+        searchCancel = (TextView) findViewById(R.id.cancel_image);
+        searchText = (SearchView) findViewById(R.id.search_text);
         viewContainer = new ArrayList<>();
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LayoutInflater i = LayoutInflater.from(MainActivity.this.getBaseContext());
-                view = getLayoutInflater().inflate(R.layout.scroll_item_view, null);
-                iv = (ImageView) view.findViewById(R.id.image_view);
+                View view = getLayoutInflater().inflate(R.layout.scroll_item_view, null);
+                ImageView iv = (ImageView) view.findViewById(R.id.image_view);
                 TextView tx = (TextView) view.findViewById(R.id.text);
                 tx.setText(viewContainer.size() + 1 + " Item");
                 linearContainer.addView(view);
@@ -62,24 +71,41 @@ public class MainActivity extends ActionBarActivity {
                     public void onClick(View v) {
                         linearContainer.removeView(viewContainer.get((Integer) v.getTag()));
                         viewContainer.remove((int) (Integer) v.getTag());
-                        refixPosition();
+                        fixPosition();
                         checkAndSetWidth();
                         mainHolder.invalidate();
                     }
                 });
 
-                findViewById(R.id.scroll).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(MainActivity.this, ScrollSampleActivity.class));
-                    }
-                });
-
             }
         });
+
+        findViewById(R.id.scroll).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScrollSampleActivity.class));
+            }
+        });
+
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                friendContainer.setVisibility(View.GONE);
+                searchContainer.setVisibility(View.VISIBLE);
+            }
+        });
+
+        searchCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                friendContainer.setVisibility(View.VISIBLE);
+                searchContainer.setVisibility(View.GONE);
+            }
+        });
+
     }
 
-    private void refixPosition() {
+    private void fixPosition() {
         for (int i = 0; i < viewContainer.size(); i++) {
             View vi = viewContainer.get(i);
             TextView tx = (TextView) vi.findViewById(R.id.text);
@@ -94,19 +120,15 @@ public class MainActivity extends ActionBarActivity {
         linearContainer.measure(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         int scrollWidth = horizontalScroll.getMeasuredWidth();
         int lineaWidth = linearContainer.getMeasuredWidth();
-        int searchWidth = searchText.getWidth();
         int mainWidth = getResources().getDisplayMetrics().widthPixels;
 
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) horizontalScroll.getLayoutParams();
-        if (searchWidth + scrollWidth > mainWidth) {
+        if (80 + scrollWidth > mainWidth) {
             params.width = oldWidth;
             horizontalScroll.setLayoutParams(params);
-        } else if (scrollWidth == lineaWidth) {
+        } else {
             params.width = scrollWidth;
             horizontalScroll.setLayoutParams(params);
-            oldWidth = scrollWidth;
-        } else {
-            params.width = mainWidth - lineaWidth;
             oldWidth = scrollWidth;
         }
 
